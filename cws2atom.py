5ecf058dd92fdbd2b49911d4010314c8237512d7
Eloston <Eloston@users.noreply.github.com>
2016-03-05 20:50:05 -0800

    Refactor code Add retrieving of replies

--- cws2atom.py
+++ cws2atom.py
@@ -10,6 +10,7 @@ import urllib.parse
 import urllib.request
 import json
 import re
+import copy
 
 import pyatom
 
@@ -44,33 +45,55 @@ def get_comments(extension_id, group):
         matches = re.match(r"window\.google\.annotations2\.component\.load\(\{'1':\{'results':\{\"annotations\":\[(?P<reviews>.+)\]\,\"numAnnotations\":[0-9]+\,\"numAnnotationsAccuracy\":[0-9]+\,", response_data)
         return json.loads("[" + matches.group("reviews") + "]")
 
-def process_reviews(extension_id, feed):
-    for review in get_comments(extension_id, "chrome_webstore"):
-        if "displayName" in review["entity"]:
-            reviewer_name = review["entity"]["displayName"]
-        else:
-            reviewer_name = "(Anonymous)"
-        feed.add(title="Review from " + reviewer_name + " - " + str(review["starRating"]) + "/5 stars",
-                content="<p>Comment: " + review["comment"] + "</p>",
-                content_type="html",
-                updated=datetime.datetime.utcfromtimestamp(review["timestamp"]),
-                url="https://chrome.google.com/webstore/detail/" + extension_id,
-                id="review" + str(review["timestamp"]) + review["entity"]["shortAuthor"] + review["comment"]
-        )
+def get_comment_replies(extension_id, comment_list):
+    request_url = "https://chrome.google.com/reviews/json/search"
+    search_query_template =             {
+        "requireComment": True,
+        "entities": [
+            {
+                "annotation": {
+                    "groups": None,
+                    "author": None,
+                    "url": "http://chrome.google.com/extensions/permalink?id=" + extension_id
+                }
+            }
+        ],
+        "matchExtraGroups": True,
+        "startIndex": 0,
+        "numResults": 100,
+        "includeNicknames": True
+    }
+    request_values = {
+        "applicationId": 94,
+        "searchSpecs": []
+    }
 
-def process_support_feedback(extension_id, feed):
-    for feedback in get_comments(extension_id, "chrome_webstore_support"):
-        if "displayName" in feedback["entity"]:
-            reporter_name = feedback["entity"]["displayName"]
-        else:
-            reporter_name = "(Anonymous)"
-        feed.add(title="Feedback from " + reporter_name + ": " + str(feedback["title"]),
-                content="<p>Comment: " + feedback["comment"] + "</p>",
-                content_type="html",
-                updated=datetime.datetime.utcfromtimestamp(feedback["timestamp"]),
-                url="https://chrome.google.com/webstore/detail/" + extension_id,
-                id="support" + str(feedback["timestamp"]) + feedback["entity"]["shortAuthor"] + feedback["comment"]
-        )
+    id_to_index = dict()
+    
+    for comment in comment_list:
+        if "replyExists" in comment["attributes"] and comment["attributes"]["replyExists"] is True:
+            search_query = copy.deepcopy(search_query_template)
+            search_query["entities"][0]["annotation"]["author"] = comment["entity"]["author"]
+            search_query["entities"][0]["annotation"]["groups"] = comment["entity"]["groups"]
+            request_values["searchSpecs"].append(search_query)
+
+    request_data = "req=" + json.dumps(request_values)
+    request_data = request_data.encode("UTF-8")
+    request = urllib.request.Request(request_url, request_data, headers={ "Content-Type": "text/plain;charset=UTF-8" })
+    with urllib.request.urlopen(request) as response:
+        response_encoding = response.headers["Content-Type"].split("charset=")[1]
+        response_data = response.read().decode(response_encoding)
+        return json.loads(response_data)
+
+def get_all_webstore_data(extension_id):
+    comment_list = get_comments(extension_id, "chrome_webstore") + get_comments(extension_id, "chrome_webstore_support")
+    comment_replies = get_comment_replies(extension_id, comment_list)
+    comments_with_replies_count = 0
+    for comment in comment_list:
+        if "replyExists" in comment["attributes"] and comment["attributes"]["replyExists"] is True:
+            comment["cws2atom_replies"] = comment_replies["searchResults"][comments_with_replies_count]["annotations"]
+            comments_with_replies_count += 1
+    return comment_list
 
 if __name__ == "__main__":
     extension_id = sys.argv[1]
@@ -96,7 +119,58 @@ if __name__ == "__main__":
 
     feed = init_feed()
 
-    process_reviews(extension_id, feed)
-    process_support_feedback(extension_id, feed)
+    comments_with_replies_list = get_all_webstore_data(extension_id)
+
+    for comment in comments_with_replies_list:
+        if "displayName" in comment["entity"]:
+            commenter_name = comment["entity"]["displayName"]
+        else:
+            commenter_name = "(Anonymous)"
+        if "chrome_webstore" in comment["entity"]["groups"]:
+            title = "Review - " + commenter_name + " - " + str(comment["starRating"]) + "/5 stars"
+            feed.add(title=title,
+                    content="<p>Comment: " + comment["comment"] + "</p>",
+                    content_type="html",
+                    updated=datetime.datetime.utcfromtimestamp(comment["timestamp"]),
+                    url="https://chrome.google.com/webstore/detail/" + extension_id,
+                    id="review" + str(comment["timestamp"]) + comment["entity"]["shortAuthor"] + comment["comment"]
+            )
+            if "cws2atom_replies" in comment:
+                for comment_reply in comment["cws2atom_replies"]:
+                    if "displayName" in comment_reply["entity"]:
+                        replier_name = comment_reply["entity"]["displayName"]
+                    else:
+                        replier_name = "(Anonymous)"
+                    feed.add(title="RE - " + replier_name + ": " + title,
+                            content="<p>Comment reply: " + comment_reply["comment"] + "</p>",
+                            content_type="html",
+                            updated=datetime.datetime.utcfromtimestamp(comment_reply["timestamp"]),
+                            url="https://chrome.google.com/webstore/detail/" + extension_id,
+                            id="review_reply" + str(comment_reply["timestamp"]) + comment_reply["entity"]["shortAuthor"] + comment_reply["comment"]
+                    )
+        elif "chrome_webstore_support" in comment["entity"]["groups"]:
+            title=comment["attributes"]["sfrAttributes"]["issueType"] + " - " + commenter_name + " - " + str(comment["title"])
+            feed.add(title=title,
+                    content="<p>App Version: " + comment["attributes"]["sfrAttributes"]["appVersion"] + "</p>" \
+                        + "<p>Client Version: " + comment["attributes"]["sfrAttributes"]["clientVersion"] + "</p>" \
+                        + "<p>Comment: " + comment["comment"] + "</p>",
+                    content_type="html",
+                    updated=datetime.datetime.utcfromtimestamp(comment["timestamp"]),
+                    url="https://chrome.google.com/webstore/detail/" + extension_id,
+                    id="support" + str(comment["timestamp"]) + comment["entity"]["shortAuthor"] + comment["comment"]
+            )
+            if "cws2atom_replies" in comment:
+                for comment_reply in comment["cws2atom_replies"]:
+                    if "displayName" in comment_reply["entity"]:
+                        replier_name = comment_reply["entity"]["displayName"]
+                    else:
+                        replier_name = "(Anonymous)"
+                    feed.add(title="RE - " + replier_name + ": " + title,
+                            content="<p>Comment reply: " + comment_reply["comment"] + "</p>",
+                            content_type="html",
+                            updated=datetime.datetime.utcfromtimestamp(comment_reply["timestamp"]),
+                            url="https://chrome.google.com/webstore/detail/" + extension_id,
+                            id="support_reply" + str(comment_reply["timestamp"]) + comment_reply["entity"]["shortAuthor"] + comment_reply["comment"]
+                    )
 
     print(feed.to_string())
